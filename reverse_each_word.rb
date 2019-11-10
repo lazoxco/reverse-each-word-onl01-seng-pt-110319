77ffdbe62beb4ff7e8c3a46db51daf765419ba6d
Nory Lazo <nlazo@icloud.com>
2019-11-10 23:55:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(words)
   reversed_words = Array.new
  
   words_arr = words.split
-  words_arr.collect do |word|
+  words_arr.each do |word|
     reversed_words << word.reverse
   end
   reversed_words.join(" ")
